0d2f160f67266436a8afae3ebbd2811e37443de6
Charles Hapner <chapner91@gmail.com>
2020-10-11 11:40:02 -0400

    stubbed out ICriteriaFunction

---
+++ Criteria/CriteriaItems/CriteriaFunctions/CriteriaFunctionOneArg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Criteria.Enums;
+
+namespace Criteria.CriteriaItems.CriteriaFunctions
+{
+	class CriteriaFunctionOneArg : ICriteriaFunction
+	{
+		public string CriteriaItemType => throw new NotImplementedException();
+		public string CriteriaItemFunctionType => throw new NotImplementedException();
+		public DataType DataType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public string Value => throw new NotImplementedException();
+
+		public List<DataType> AcceptedDataTypes => throw new NotImplementedException();
+
+		public string Serialize()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Criteria/CriteriaItems/CriteriaFunctions/ICriteriaFunction.cs
@@ -0,0 +1,15 @@
+﻿using Criteria.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criteria.CriteriaItems.CriteriaFunctions
+{
+	public interface ICriteriaFunction : ICriteriaItem
+	{
+		string CriteriaItemFunctionType { get; }
+		List<DataType> AcceptedDataTypes { get; }  
+	}
+}
